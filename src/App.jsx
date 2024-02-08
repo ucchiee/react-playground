4c65440ab00f97028b8d8e800e2f25de9e8e6f2b
Yusuke Uchida <fantomyuu0623@gmail.com>
2024-02-08 11:49:31 +0900

    material init

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-	return (
-		<div className="drawer lg:drawer-open">
-			<input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-			<div className="drawer-content flex flex-col items-center justify-center">
-				{/* Page content here */}
-				<label
-					htmlFor="my-drawer-2"
-					className="btn btn-primary drawer-button lg:hidden"
-				>
-					Open drawer
-				</label>
-			</div>
-			<div className="drawer-side">
-				<label
-					htmlFor="my-drawer-2"
-					aria-label="close sidebar"
-					className="drawer-overlay"
-				></label>
-				<ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-					{/* Sidebar content here */}
-					<input
-						type="text"
-						placeholder="Type here"
-						className="input input-bordered w-full max-w-xs"
-					/>
-					<li>
-						<a>Sidebar Item 1</a>
-					</li>
-					<li>
-						<a>Sidebar Item 2</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
